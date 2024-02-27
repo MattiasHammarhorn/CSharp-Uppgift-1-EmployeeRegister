71d6aa91fa2bd79e84ff082e037d960c0fa060b1
Mattias Hammarhorn <mattias@amuze-interactive.com>
2024-02-27 16:29:59 +0100

    Separated Employee class to its own file

---
+++ EmployeeRegister/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeRegister
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Wage { get; set; }
+
+        public string FullName()
+        {
+            return this.FirstName + " " + this.LastName;
+        }
+    }
+}
--- EmployeeRegister/Program.cs
+++ EmployeeRegister/Program.cs
@@ -97,17 +97,4 @@
             Console.ReadKey();
         }
     }
-
-    internal class Employee
-    {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public decimal Wage { get; set; }
-
-        public string FullName()
-        {
-            return this.FirstName + " " + this.LastName;
-        }
-    }
 }
